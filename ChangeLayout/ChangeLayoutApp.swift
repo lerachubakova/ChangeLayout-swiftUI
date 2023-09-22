2d902e290a47895e423b601fbdf361851a8595d9
Valeriya <Chubakova>
2023-09-22 15:47:30 +0300

    Initial Commit

---
+++ ChangeLayout/ChangeLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeLayoutApp.swift
+//  ChangeLayout
+//
+//  Created by Valeriya Chubakova on 22.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ChangeLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
